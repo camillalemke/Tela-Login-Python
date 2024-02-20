a3bbe1a81e880476c6a01de931c123f8d41eb937
Camilla Santana <159941222+camillalemke@users.noreply.github.com>
2024-02-20 17:01:13 -0300

    Tela de Login em Python

---
+++ pysimplegui.py
@@ -0,0 +1,27 @@
+import PySimpleGUI as sg
+
+layout = [
+    [sg.Text('Usuario')],
+    [sg.Input(key='usuario')],
+    [sg.Text('Senha')],
+    [sg.Input(key='senha')],
+    [sg.Button('login')],
+    [sg.Text('', key='mensagem')]
+]
+
+window = sg.Window('Login',layout=layout)
+
+while True:
+    event, values = window.read()
+    if event == sg.WIN_CLOSED:
+        break
+    elif event == 'login':
+     usuario = values['usuario']
+     senha_correta = '123456'
+     usuario_correto = 'jhonatan'
+     usuario = values['usuario']
+     senha =  values['senha']
+     if senha == senha_correta and usuario == usuario_correto:
+         window['mensagem'].update('Login feito com sucesso')
+     else:
+         window['mensagem'].update('Login ou senha incorreto')
